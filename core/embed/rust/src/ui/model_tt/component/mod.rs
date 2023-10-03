9f3f8a22dd3277e2ab1ef4ac492349b438b9eb1e
grdddj <jiri.musil06@seznam.cz>
2023-10-03 08:20:27 +0200

    WIP - minimize the usage of #[cfg(feature = "micropython")] outside translations module

--- core/embed/rust/src/ui/model_tt/component/mod.rs
+++ core/embed/rust/src/ui/model_tt/component/mod.rs
@@ -1,10 +1,5 @@
-// NOTE: things needing micropython do so mostly because of translations
-// (we cannot have them inside bootloader)
-
-#[cfg(feature = "micropython")]
 mod address_details;
 mod button;
-#[cfg(feature = "micropython")]
 mod coinjoin_progress;
 mod dialog;
 mod fido;
@@ -12,16 +7,13 @@ mod fido;
 mod fido_icons;
 mod error;
 mod frame;
-#[cfg(feature = "micropython")]
 mod hold_to_confirm;
 #[cfg(feature = "dma2d")]
 mod homescreen;
 mod horizontal_page;
 mod keyboard;
 mod loader;
-#[cfg(feature = "micropython")]
 mod number_input;
-#[cfg(feature = "micropython")]
 mod page;
 mod progress;
 mod result;
@@ -29,19 +21,16 @@ mod scroll;
 mod swipe;
 mod welcome_screen;
 
-#[cfg(feature = "micropython")]
 pub use address_details::AddressDetails;
 pub use button::{
     Button, ButtonContent, ButtonMsg, ButtonStyle, ButtonStyleSheet, CancelConfirmMsg,
     CancelInfoConfirmMsg, IconText, SelectWordMsg,
 };
-#[cfg(feature = "micropython")]
 pub use coinjoin_progress::CoinJoinProgress;
 pub use dialog::{Dialog, DialogMsg, IconDialog};
 pub use error::ErrorScreen;
 pub use fido::{FidoConfirm, FidoMsg};
 pub use frame::{Frame, FrameMsg};
-#[cfg(feature = "micropython")]
 pub use hold_to_confirm::{HoldToConfirm, HoldToConfirmMsg};
 #[cfg(feature = "dma2d")]
 pub use homescreen::{Homescreen, HomescreenMsg, Lockscreen};
@@ -55,9 +44,7 @@ pub use keyboard::{
     word_count::{SelectWordCount, SelectWordCountMsg},
 };
 pub use loader::{Loader, LoaderMsg, LoaderStyle, LoaderStyleSheet};
-#[cfg(feature = "micropython")]
 pub use number_input::{NumberInputDialog, NumberInputDialogMsg};
-#[cfg(feature = "micropython")]
 pub use page::{SwipeHoldPage, SwipePage};
 pub use progress::Progress;
 pub use result::{ResultFooter, ResultScreen, ResultStyle};
